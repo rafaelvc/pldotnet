1fa91107e013b4222a1ccebeacbdef18a07ff041
Rafael Cabral <verganic@gmail.com>
2019-09-27 13:57:59 -0300

    - more required code from dotnet-embedded-sample

--- pldotnet.c
+++ pldotnet.c
@@ -2,6 +2,8 @@
 
 //// DOTNET-HOST-SAMPLE STUFF ///////////////////////////////////////
 // Provided by the AppHost NuGet package and installed as an SDK pack
+#include <assert.h>
+
 #include <nethost.h>
 // Header files copied from https://github.com/dotnet/core-setup
 #include <coreclr_delegates.h>
@@ -235,10 +237,82 @@ Datum pldotnet_inline_handler(PG_FUNCTION_ARGS)
         elog(ERROR, "[pldotnet]: could not disconnect from SPI manager");
     PG_RETURN_VOID();
 }
+
+//// DOTNET-HOST-SAMPLE STUFF ///////////////////////////////////////
+/********************************************************************************************
+ * Function used to load and activate .NET Core
+ ********************************************************************************************/
+
+// Forward declarations
+void *load_library(const char_t *);
+void *get_export(void *, const char *);
+
+void *load_library(const char_t *path)
+{
+    fprintf(stderr, "# DEBUG: doing dlopen(%s).\n", path);
+    void *h = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
+    assert(h != nullptr);
+    return h;
+}
+void *get_export(void *h, const char *name)
+{
+    void *f = dlsym(h, name);
+    if(f == nullptr){
+        fprintf(stderr, "Can't dlsym(%s); exiting.\n", name);
+        exit(-1);
+    }
+    return f;
+}
+
+// <SnippetLoadHostFxr>
+// Using the nethost library, discover the location of hostfxr and get exports
+int load_hostfxr()
+{
+    // Pre-allocate a large buffer for the path to hostfxr
+    char_t buffer[MAX_PATH];
+    size_t buffer_size = sizeof(buffer) / sizeof(char_t);
+    int rc = get_hostfxr_path(buffer, &buffer_size, nullptr);
+    if (rc != 0)
+        return 0;
+
+    // Load hostfxr and get desired exports
+    void *lib = load_library(buffer);
+    init_fptr = (hostfxr_initialize_for_runtime_config_fn)get_export( \
+        lib, "hostfxr_initialize_for_runtime_config");
+    get_delegate_fptr = (hostfxr_get_runtime_delegate_fn)get_export( \
+        lib, "hostfxr_get_runtime_delegate");
+    close_fptr = (hostfxr_close_fn)get_export(lib, "hostfxr_close");
+
+    return (init_fptr && get_delegate_fptr && close_fptr);
+}
+// </SnippetLoadHostFxr>
+
+// <SnippetInitialize>
+// Load and initialize .NET Core and get desired function pointer for scenario
+load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly( \
+    const char_t *config_path)
+{
+    // Load .NET Core
+    void *load_assembly_and_get_function_pointer = nullptr;
+    hostfxr_handle cxt = nullptr;
+    int rc = init_fptr(config_path, nullptr, &cxt);
+    if (rc != 0 || cxt == nullptr)
+    {
+        fprintf(stderr, "Init failed: %x\n", rc);
+        close_fptr(cxt);
+        return nullptr;
+    }
+
+    // Get the load assembly function pointer
+    rc = get_delegate_fptr(
+        cxt,
+        hdt_load_assembly_and_get_function_pointer,
+        &load_assembly_and_get_function_pointer);
+    if (rc != 0 || load_assembly_and_get_function_pointer == nullptr)
+        fprintf(stderr, "Get delegate failed: %x\n", rc);
+    close_fptr(cxt);
+    return (load_assembly_and_get_function_pointer_fn)load_assembly_and_get_function_pointer;
+}
+// </SnippetInitialize>
+//// DOTNET-HOST-SAMPLE STUFF ///////////////////////////////////////
 #endif
-
-
-
-
-
-
