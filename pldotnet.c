7579c7745545d2cd5eadd895d7acdbbb773173de
todd lewis <todd.0.lewis@gmail.com>
2019-09-27 18:02:16 -0300

    1) Convert function scope to static; this is C best practice. 2) Put function names in column 1, per PSQL convention. 3) Use "pldotnet" prefix, following practice in pltcl and elsewhere.

--- pldotnet.c
+++ pldotnet.c
@@ -24,8 +24,8 @@ static hostfxr_get_runtime_delegate_fn get_delegate_fptr;
 static hostfxr_close_fn close_fptr;
 
 // Forward declarations
-int load_hostfxr();
-load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t *assembly);
+static int load_hostfxr();
+static load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t *assembly);
 
 // TODO: Check how Postgres API do this or shoud we use C lib
 #define SNPRINTF(name, size, fmt, ...)                  \
@@ -249,17 +249,20 @@ Datum pldotnet_inline_handler(PG_FUNCTION_ARGS)
  ********************************************************************************************/
 
 // Forward declarations
-void *load_library(const char_t *);
-void *get_export(void *, const char *);
+static void *pldotnet_load_library(const char_t *);
+static void *pldotnet_get_export(void *, const char *);
 
-void *load_library(const char_t *path)
+static void *
+pldotnet_load_library(const char_t *path)
 {
     fprintf(stderr, "# DEBUG: doing dlopen(%s).\n", path);
     void *h = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
     assert(h != nullptr);
     return h;
 }
-void *get_export(void *h, const char *name)
+
+static void *
+pldotnet_get_export(void *h, const char *name)
 {
     void *f = dlsym(h, name);
     if(f == nullptr){
@@ -271,7 +274,8 @@ void *get_export(void *h, const char *name)
 
 // <SnippetLoadHostFxr>
 // Using the nethost library, discover the location of hostfxr and get exports
-int load_hostfxr()
+static int
+load_hostfxr()
 {
     // Pre-allocate a large buffer for the path to hostfxr
     char_t buffer[MAX_PATH];
@@ -281,12 +285,12 @@ int load_hostfxr()
         return 0;
 
     // Load hostfxr and get desired exports
-    void *lib = load_library(buffer);
-    init_fptr = (hostfxr_initialize_for_runtime_config_fn)get_export( \
+    void *lib = pldotnet_load_library(buffer);
+    init_fptr = (hostfxr_initialize_for_runtime_config_fn)pldotnet_get_export( \
         lib, "hostfxr_initialize_for_runtime_config");
-    get_delegate_fptr = (hostfxr_get_runtime_delegate_fn)get_export( \
+    get_delegate_fptr = (hostfxr_get_runtime_delegate_fn)pldotnet_get_export( \
         lib, "hostfxr_get_runtime_delegate");
-    close_fptr = (hostfxr_close_fn)get_export(lib, "hostfxr_close");
+    close_fptr = (hostfxr_close_fn)plpldotnet_get_export(lib, "hostfxr_close");
 
     return (init_fptr && get_delegate_fptr && close_fptr);
 }
@@ -294,8 +298,8 @@ int load_hostfxr()
 
 // <SnippetInitialize>
 // Load and initialize .NET Core and get desired function pointer for scenario
-load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly( \
-    const char_t *config_path)
+static load_assembly_and_get_function_pointer_fn 
+get_dotnet_load_assembly(const char_t *config_path)
 {
     // Load .NET Core
     void *load_assembly_and_get_function_pointer = nullptr;
@@ -318,6 +322,7 @@ load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly( \
     close_fptr(cxt);
     return (load_assembly_and_get_function_pointer_fn)load_assembly_and_get_function_pointer;
 }
+
 // </SnippetInitialize>
 //// DOTNET-HOST-SAMPLE STUFF ///////////////////////////////////////
 #endif
