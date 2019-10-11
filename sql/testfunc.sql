945cd9e982c0ac34d0b45ba52237b6a3ba09893d
Rafael Cabral <verganic@gmail.com>
2019-10-11 20:08:24 -0300

    - Review pointer arithmetics - first run of a   function with ONE param

--- sql/testfunc.sql
+++ sql/testfunc.sql
@@ -1,4 +1,24 @@
-CREATE OR REPLACE FUNCTION returnX() RETURNS integer AS $$
-return 10;
-$$ LANGUAGE pldotnet;
-SELECT returnX();
+--CREATE OR REPLACE FUNCTION returnX() RETURNS integer AS $$
+--return 10;
+--$$ LANGUAGE pldotnet;
+--SELECT returnX();
+
+
+--CREATE OR REPLACE FUNCTION returnSum() RETURNS integer AS $$
+--return a+b;
+--$$ LANGUAGE pldotnet;
+--SELECT returnSum();
+
+
+CREATE OR REPLACE FUNCTION inc2(val integer) RETURNS integer AS $$
+return val + 1;
+$$
+LANGUAGE pldotnet;
+SELECT inc2(8);
+
+
+-- CREATE OR REPLACE FUNCTION returnSUM(a integer, b integer) RETURNS integer AS $$
+-- return a + b;
+-- $$
+-- LANGUAGE pldotnet;
+-- SELECT returnSUM(2,3);
