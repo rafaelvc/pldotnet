59a6e59d05f9cf3d2c106fdedd33baba9e8a5a41
Lucas Evangelista Alves Peixoto <lpx@briackabode.com>
2019-10-17 17:16:33 -0300

    Add Roslyn compilation to DO function

---
+++ DotNetLib/src/Lib.cs
@@ -0,0 +1,91 @@
+using System;   
+using System.Collections.Generic;          
+using System.Globalization;                
+using System.IO;                           
+using System.Linq;                         
+using System.Reflection;                   
+using System.Runtime.InteropServices;      
+using Microsoft.CodeAnalysis;              
+using Microsoft.CodeAnalysis.CSharp;       
+using Microsoft.CodeAnalysis.Text;         
+                                           
+namespace DotNetLib                        
+{                                          
+    public static class Lib                
+    {                                      
+	[StructLayout(LayoutKind.Sequential)]  
+        public struct LibArgs              
+        {                                  
+            public IntPtr SourceCode;      
+            public int Number;             
+        }                                  
+                                           
+	static MemoryStream memStream;         
+                                           
+        public static int Compile(IntPtr arg, int argLength)
+        {                                  
+            LibArgs libArgs = Marshal.PtrToStructure<LibArgs>(arg);
+
+            string sourceCode = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? Marshal.PtrToStringUni(libArgs.SourceCode)
+                : Marshal.PtrToStringUTF8(libArgs.SourceCode); 
+           
+            //Console.WriteLine($"Source Code: {sourceCode}");
+	    SyntaxTree tree = SyntaxFactory.ParseSyntaxTree(sourceCode);
+
+	    var trustedAssembliesPaths = ((string)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")).Split(Path.PathSeparator);
+
+	    var neededAssemblies = new[]
+	    {
+	        "System.Runtime",
+	        "System.Private.CoreLib",
+	        "System.Console",
+	    };
+
+	    List<PortableExecutableReference> references = trustedAssembliesPaths
+                .Where(p => neededAssemblies.Contains(Path.GetFileNameWithoutExtension(p)))
+                .Select(p => MetadataReference.CreateFromFile(p))
+		.ToList();
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                "plnetproc.dll",
+		options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
+                syntaxTrees: new[] { tree },
+                references: references);
+                                           
+            Lib.memStream = new MemoryStream();
+            Microsoft.CodeAnalysis.Emit.EmitResult compileResult = compilation.Emit(Lib.memStream);
+
+	    if(!compileResult.Success)
+	    {
+	        Console.WriteLine("\n********ERROR************\n");
+	        foreach(var diagnostic in compileResult.Diagnostics)
+	        {
+	            Console.WriteLine(diagnostic.ToString());
+	        }
+	        Console.WriteLine("\n********ERROR************\n");
+	    }
+
+	    string path = "CompiledProcCode.dll";
+	    using(FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
+	    {                                  
+                Microsoft.CodeAnalysis.Emit.EmitResult compileResultFile = compilation.Emit(stream);
+	    }                                  
+                                           
+	    return 0;
+	}                                      
+                                           
+	public static int Run(IntPtr arg, int argLength)
+	{                                      
+            Assembly compiledAssembly;     
+            compiledAssembly = Assembly.Load(Lib.memStream.GetBuffer());
+          	                               
+	    Type procClassType = compiledAssembly.GetType("ProcedureCode.ProcedureClass");
+            MethodInfo procMethod = calculator.GetMethod("ProcedureMethod");
+            procMethod.Invoke(null, null);
+                                           
+ 	    return 0;                          
+        }                                  
+    }                                      
+}
+
