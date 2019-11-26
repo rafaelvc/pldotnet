a4b1e16745f9671f54bbdad53b19fdb425fc9dcc
Lucas Peixoto <lpx@briackabode.com>
2019-11-26 14:58:52 -0300

    WIP - Fixed C# test for separated languages

--- sql/testdo.sql
+++ sql/testdo.sql
@@ -1,6 +1,6 @@
 do $$
 	int sumResult = 3 + 7;
 	Console.WriteLine($"Compiled Sum: {sumResult}");
-$$ language plcsharp;
+$$ language pldotnet;
 
 SELECT 1;
--- sql/testfloats.sql
+++ sql/testfloats.sql
@@ -2,20 +2,20 @@
 CREATE OR REPLACE FUNCTION returnReal() RETURNS real AS $$
 return 1.50055f;
 $$ LANGUAGE plcsharp;
-SELECT returnReal();
+SELECT returnReal() = real '1.50055';
 
 CREATE OR REPLACE FUNCTION sumReal(a real, b real) RETURNS real AS $$
 return a+b;
 $$ LANGUAGE plcsharp;
-SELECT sumReal(1.50055, 1.50054); -- 3.00109
+SELECT sumReal(1.50055, 1.50054) = real '3.00109'; -- 3.00109
 
 --- Float8 (double precision): 15 digits of precison
 CREATE OR REPLACE FUNCTION returnDouble() RETURNS double precision AS $$
 return 11.0050000000005;
 $$ LANGUAGE plcsharp;
-SELECT returnDouble();
+SELECT returnDouble() = double precision '11.0050000000005';
 
 CREATE OR REPLACE FUNCTION sumDouble(a double precision, b double precision) RETURNS double precision AS $$
 return a+b;
 $$ LANGUAGE plcsharp;
-SELECT sumDouble(10.5000000000055, 10.5000000000054); -- 21.0000000000109
+SELECT sumDouble(10.5000000000055, 10.5000000000054) = double precision  '21.0000000000109'; -- 21.0000000000109
