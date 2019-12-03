646acd9cb7349ef9f0bd8da5d550b1a43cb2aed4
Lucas Peixoto <lpx@briackabode.com>
2019-12-03 17:43:19 -0300

    Add C# naming and modules
            - Add C# related files
            - Add Hostfxr related files
            - Remove debug logs

--- pldotnet.h
+++ pldotnet.h
@@ -7,12 +7,5 @@
 #ifndef PLDOTNET_H
 #define PLDOTNET_H
 
-#include "pldotnet_common.h"
-
 #endif // PLDOTNET_H
 
-//Datum DotNET_callhandler (/*additional args,*/ FunctionCallInfo fcinfo);
-//Datum DotNET_validator (/*additional args,*/ Oid oid);
-//#if PG_VERSION_NUM >= 90000
-//Datum DotNET_inlinehandler (/*additional args,*/ const char *source);
-//#endif
---
+++ pldotnet_csharp.h
@@ -0,0 +1,7 @@
+#ifndef PLCSHARP_H
+#define PLCSHARP_H
+
+#include "pldotnet_common.h"
+
+#endif  /* PLFCHARP_H */
+
