c36ea5a53c90b0ff8d295f1aaa40eaf636c0c3a9
Lucas Peixoto <lpx@briackabode.com>
2020-01-10 15:44:36 -0300

    V11 - Attempt to marshall numeric returned from SPI

--- sql/testspi.sql
+++ sql/testspi.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE FUNCTION returnSelect2() RETURNS integer AS $$
-return PlDotNet.CExecute("SELECT CAST(1.2 as real);", 1);
+return PlDotNet.CExecute("SELECT 1.2", 1);
 $$ LANGUAGE plcsharp;
 SELECT returnSelect2();
