2c3b97d9b6b9aad32f79743d29a2e5cf09b3d7e3
Lucas Peixoto <lpx@briackabode.com>
2020-01-14 14:41:50 -0300

    V14 - Attempt to return ExpandoObject to procedure

--- sql/testspi.sql
+++ sql/testspi.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE FUNCTION returnSelect2() RETURNS integer AS $$
-return PlDotNet.CExecute("SELECT CAST(1.2 as real)", 1);
+CREATE OR REPLACE FUNCTION returnSelect2() RETURNS double precision AS $$
+var exp = PlDotNet.SPIExecute("SELECT CAST(11.0050000000005 as double precision) as column", 1);
+Console.WriteLine(exp.Count);
+return 1.2;
 $$ LANGUAGE plcsharp;
 SELECT returnSelect2();
