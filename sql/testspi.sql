148398294349005319aef5cd8759ba1abf3e25c8
Lucas Peixoto <lpx@briackabode.com>
2020-01-15 16:42:17 -0300

    V15 - Set native lib path at runtime for invoking

--- sql/testspi.sql
+++ sql/testspi.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION returnSelect2() RETURNS double precision AS $$
+CREATE OR REPLACE FUNCTION returnSelect2() RETURNS integer AS $$
 var exp = PlDotNet.SPIExecute("SELECT 1 as c, 2 as b", 1);
 Console.WriteLine("start");
 foreach (var row in exp) {
@@ -6,6 +6,6 @@ foreach (var row in exp) {
     Console.WriteLine(row.c);
 }
 Console.WriteLine("end");
-return 1.2;
+return exp[0].b;
 $$ LANGUAGE plcsharp;
 SELECT returnSelect2();
